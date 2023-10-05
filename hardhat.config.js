d99deb9796b31a5917f6cf882717689d29b51bef
john j <143453887+JohnDeveloperJ@users.noreply.github.com>
2023-10-05 15:49:30 +0000

    modified:   hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const ALCHEMY_API_KEY = "gaCXiUJlvYUlSqK2gNLbVD46OfSt_ldE";
-const SEPOLIA_PRIVATE_KEY = "562eb4d874fde371080697d03a9f11465ec663cec9311c75e64939e50eb088bd";
+const ALCHEMY_API_KEY = "";
+const SEPOLIA_PRIVATE_KEY = "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
