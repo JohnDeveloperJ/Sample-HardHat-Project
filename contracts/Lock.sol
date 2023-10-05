06d3cfafa8aa80fafe6dd054742b1ba05851f33c
john j <143453887+JohnDeveloperJ@users.noreply.github.com>
2023-10-05 15:56:22 +0000

    modified:   contracts/Lock.sol modified:   hardhat.config.js

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-
- contract Lock {
+contract Lock {
     uint public unlockTime;
     address payable public owner;
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,14 +5,14 @@ const SEPOLIA_PRIVATE_KEY = "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.19",
-    etherscan: {
-        apiKey: "QHSPPNJMP3QECU7Q8XNQH4CYJ5QWBYZKT7",
+  solidity: "0.8.19",
+  etherscan: {
+    apiKey: "QHSPPNJMP3QECU7Q8XNQH4CYJ5QWBYZKT7",
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
     },
-    networks: {
-        sepolia: {
-            url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-            accounts: [SEPOLIA_PRIVATE_KEY],
-        },
-    },
+  },
 };
